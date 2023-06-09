ac61b8acff8218369c0bb71ecaf6defb5f56c168
s1300183 <s130183@u-aizu.ac.jp>
2023-06-09 13:58:00 +0900

    Won/lost message added

---
+++ Dice.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+class Dice{
+    public static void main(String[] args){
+	int saikoro1;
+	int saikoro2;
+	Scanner sc = new Scanner(System.in);
+
+	Random random = new Random();
+	saikoro1 = random.nextInt(6)+1;
+	saikoro2 = random.nextInt(6)+1;
+        int sum = saikoro1 + saikoro2;
+
+	
+	System.out.println("What is your name?");
+        String name = sc.next();
+	System.out.println("Hello, " + name + "!");
+
+
+	
+	System.out.println("Rolling dice...");
+	System.out.println("Die 1: " + saikoro1);
+        System.out.println("Die 2: " + saikoro2);
+	System.out.println("Total value: " + sum);
+	if(sum>=7){
+	    System.out.println("You" +" won!");
+
+	}else{
+
+		    System.out.println("You" +" lose!");
+	}
+
+    } 
+
+}
