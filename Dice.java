d8dec00b89f151ee8f5ecd094f33360012a5f2d1
s1300183 <s130183@u-aizu.ac.jp>
2023-06-09 13:24:00 +0900

    Greetings added

--- Dice.java
+++ Dice.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 
 
@@ -6,12 +7,20 @@ class Dice{
     public static void main(String[] args){
 	int saikoro1;
 	int saikoro2;
+	Scanner sc = new Scanner(System.in);
 
 	Random random = new Random();
 	saikoro1 = random.nextInt(6)+1;
 	saikoro2 = random.nextInt(6)+1;
         int sum = saikoro1 + saikoro2;
 
+	
+	System.out.println("What is your name?");
+        String name = sc.next();
+	System.out.println("Hello, " + name + "!");
+
+
+	
 	System.out.println("Rolling dice...");
 	System.out.println("Die 1: " + saikoro1);
         System.out.println("Die 2: " + saikoro2);
